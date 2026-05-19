4abadbc3070da269d09001e892a6e03157535d53
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-19 09:27:34 -0400

    make interview and reject count live

--- js/jobList.js
+++ js/jobList.js
@@ -11,17 +11,28 @@ document.addEventListener('DOMContentLoaded', jobCount());
 function status(button, decision){
 
     const jobCard = button.parentElement.parentElement;
-    const statusBtn = jobCard.querySelector('.job-status')
+    const statusBtn = jobCard.querySelector('.job-status');
 
     if(decision === 'interview'){
-        statusBtn.innerText = 'INTERVIEW'
+        statusBtn.innerText = 'INTERVIEW';
         statusBtn.className = `interview job-status py-2 px-3  text-white bg-[#10B981]/75 rounded-sm`;
-        jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#10B981]`
+        jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#10B981]`;
     }
     if(decision === 'rejected'){
-        statusBtn.innerText = 'INTERVIEW'
+        statusBtn.innerText = 'INTERVIEW';
         statusBtn.className = `rejected job-status py-2 px-3  text-white bg-[#EF4444]/75 rounded-sm`;
-        jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#EF4444]`
+        jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#EF4444]`;
     }
     
+}
+
+function statusList(){
+    const interviewList = document.querySelectorAll('.interview').length;
+    const rejectList = document.querySelectorAll('.rejected').length;
+
+    const interviewCount = document.getElementById('interview-count');
+    interviewCount.textContent = interviewList;
+
+    const rejectCount = document.getElementById('reject-count');
+    rejectCount.textContent = rejectList;
 }
