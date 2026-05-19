dc1a0d34983e8d4ea5f8416c1e2ce3f2fb53e2e5
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-19 11:46:41 -0400

    use data- attribute to set job status

--- js/jobList.js
+++ js/jobList.js
@@ -6,7 +6,7 @@ function jobCount(){
 
 }
 
-document.addEventListener('DOMContentLoaded', jobCount());
+document.addEventListener('DOMContentLoaded', jobCount);
 
 function status(button, decision){
 
@@ -15,21 +15,23 @@ function status(button, decision){
 
     if(decision === 'interview'){
         statusBtn.innerText = 'INTERVIEW';
-        statusBtn.className = `interview job-status py-2 px-3  text-white bg-[#10B981]/75 rounded-sm`;
+        jobCard.dataset.status = "interview"
+        statusBtn.className = `job-status py-2 px-3  text-white bg-[#10B981]/75 rounded-sm`;
         jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#10B981]`;
     }
     if(decision === 'rejected'){
-        statusBtn.innerText = 'INTERVIEW';
-        statusBtn.className = `rejected job-status py-2 px-3  text-white bg-[#EF4444]/75 rounded-sm`;
+        statusBtn.innerText = 'REJECTED';
+        jobCard.dataset.status = "rejected"
+        statusBtn.className = `job-status py-2 px-3  text-white bg-[#EF4444]/75 rounded-sm`;
         jobCard.className = `space-y-6 p-6 bg-[#ffffff] rounded-lg hover:translate-y-1 transition-all duration-200 border-s-4 border-[#EF4444]`;
     }
     
 }
 
 function statusList(){
-    const interviewList = document.querySelectorAll('.interview').length;
-    const rejectList = document.querySelectorAll('.rejected').length;
-
+    const interviewList = document.querySelectorAll('[data-status="interview"]').length
+    const rejectList = document.querySelectorAll('[data-status="rejected"]').length
+    
     const interviewCount = document.getElementById('interview-count');
     interviewCount.textContent = interviewList;
 
